84bb0566f71519291b5e79a546ab384d48f08982
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-31 14:16:01 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -6,6 +6,46 @@ def game_hash
       :colors => ["Black", "White"],
       :players => {
         "Alan Anderson" => {
+        :number => 0
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      },
+      "Reggie Evans" => {
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      },
+      "Brook Lopez" => {
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      },
+      "Mason Plumlee" => {
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      },
+      "Jason Terry" => {
         :number =>
         :shoe =>
         :points =>
@@ -15,6 +55,7 @@ def game_hash
         :blocks =>
         :slam_dunks =>
       }
+     
       
       },
     
