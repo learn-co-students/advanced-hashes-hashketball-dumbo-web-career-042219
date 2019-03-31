fe16eef2961f1184188460e2dcbe91c8f211bec6
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-31 13:57:29 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -2,14 +2,14 @@ def game_hash
   game = 
     {
      :home => {
-      :team_name => "Brooklyn Nets"
-      :colors => ["Black", "White"]
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => {}
       },
     
       :away => {
-       :team_name => "Charlotte Hornets"
-       :colors => ["Turquoise", "Purple"]
+       :team_name => "Charlotte Hornets",
+       :colors => ["Turquoise", "Purple"],
        :players => {}
     }
 
