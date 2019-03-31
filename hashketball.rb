71e4cdbebb39ea0db62509bf4de79c55457b6cde
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-31 14:35:48 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -55,14 +55,65 @@ def game_hash
         :blocks => 11,
         :slam_dunks => 1,
       }
-     
+      }
       
       },
     
       :away => {
        :team_name => "Charlotte Hornets",
        :colors => ["Turquoise", "Purple"],
-       :players => {}
+       :players => {
+        "Jeff Adrien" => {
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2,
+      },
+      "Bismak Biyombo" => {
+        :number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 7,
+        :blocks => 15,
+        :slam_dunks => 10,
+      },
+      "Brook Lopez" => {
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15,
+      },
+      "Mason Plumlee" => {
+        :number => 1,
+        :shoe => 19,
+        :points => 26,
+        :rebounds => 12,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5,
+      },
+      "Jason Terry" => {
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1,
+      }
+      }
     }
 
   }
